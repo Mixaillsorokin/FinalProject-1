1dac34f6f0426ee3af8ddcf07ab59f972ef9dec0
Sorokin Mikhail <ip.sorokin1738@mail.ru>
2022-10-05 19:41:56 +0300

    Wrote the program code

--- TaskCode/Program.cs
+++ TaskCode/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string[] arr1 = new string[4] { "2", "hello", "world", ":-)" };
+string[] arr2 = new string[arr1.Length];
+int j = 0;
+for (int i = 0; i < arr1.Length; i++)
+{
+    if (arr1[i].Length <= 3)
+    {
+        arr2[j] = arr1[i];
+        j++;
+    }
+}
+
+void PrintArray(string[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.Write($" {arr[i]} ");
+    }
+    Console.WriteLine();
+}
+PrintArray(arr2);
